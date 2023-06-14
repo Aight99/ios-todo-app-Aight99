4a1a6e04c76af9687aa373cfff8744691d53bf71
John Doe <ltyrepj2001@gmail.com>
2023-06-15 01:49:09 +1000

    Initial commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by ios_developer on 15.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
